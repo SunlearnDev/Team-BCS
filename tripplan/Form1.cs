51b41f3809820ec76848e684f45d4791e6d43fab
Tai-TC <taitc.1440101226679@vtc.edu.vn>
2023-04-19 13:56:08 +0700

    them winfrom demo

---
+++ tripplan/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace App_TrisPlan
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        MySqlCommandBuilder cmd;
+        MySqlDataAdapter adap;
+        DataTable app;
+        string strconn = "Server=localhost;Database=app_tripplanner;Port = 3306;User ID=root;Password=";
+        string query_select = "SELECT * FROM hotel;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new MySqlConnection(strconn);
+                conn.Open();
+                adap = new MySqlDataAdapter(query_select, conn);
+                cmd = new MySqlCommandBuilder(adap);
+                app = new DataTable();
+                adap.Fill(app);
+                conn.Close();
+                dataGridView1.DataSource = app;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Lỗi kết nối MySQL: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
